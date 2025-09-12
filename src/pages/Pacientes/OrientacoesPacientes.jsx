26ba008f282307947a02205dacf9011f8e73cdd9
camilaalmeida-receitas <pedromartinstests@gmail.com>
2025-09-12 19:25:46 -0300

    Att orientacoes

--- src/pages/Pacientes/OrientacoesPacientes.jsx
+++ src/pages/Pacientes/OrientacoesPacientes.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext, useCallback } from 'react';
+import React, { useState, useEffect, useContext, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Card from '../../components/Card';
